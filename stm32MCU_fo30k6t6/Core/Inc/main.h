164e94b3479b1a27e01a192fcc11d6738c89e25c
SaumilTandon <saumiltandon1205@gmail.com>
2026-06-01 16:06:02 +0530

    relay+rs485 checked

--- stm32MCU_fo30k6t6/Core/Inc/main.h
+++ stm32MCU_fo30k6t6/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define RS485_DE_RE_Pin GPIO_PIN_8
+#define RS485_DE_RE_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
